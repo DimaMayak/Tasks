b3653b0390f1b839fc8cc008ea3b3016ba908db0
unknown <lbvbxlbvbx@gmail.com>
2020-03-26 15:50:20 +0200

    First variant of reverse LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList linkedList=new LinkedList();
-        Node node1= new Node(10);
+        LinkedList linkedList = new LinkedList();
+        Node node1 = new Node(10);
         linkedList.addElement(node1);
         linkedList.addElement(new Node(5));
         linkedList.addElement(new Node(6));
         linkedList.addElement(new Node(7));
         linkedList.addElement(new Node(8));
-        linkedList.out();
-        LinkedList list2 = new LinkedList();
-        System.out.println(linkedList.getLengthOfList());
-        linkedList.out();
-        System.out.println(linkedList.getElementById(1));
+        linkedList.getElementById(2);
+        new LinkedList();
+        LinkedList rev;
+        rev = linkedList.reverse();
+        rev.out();
     }
 }
